e2709b70bff99da792eb65ab32d5d5ec5888d785
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-09 12:34:32 +0100

    TeamMemeber's methods

--- App/Entities/TeamMember.php
+++ App/Entities/TeamMember.php
@@ -1,14 +1,27 @@
 <?php
 
-class TeamMember
+abstract class TeamMember
 {
     public function __construct(
         private int $id,
         private string $username,
         private string $email,
+        private string $passwordHash,
         private string $teamId,
         private string $createdAt
     ) {}
 
-    
+    abstract public function canCreateProject(): bool;
+    abstract public function canAssignTasks(): bool;
+    abstract public function getRolePermissions(): array;
+
+    public function verifyPassword(string $password): bool
+    {
+        return password_verify($password, $this->passwordHash);
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->passwordHash = password_hash($password, PASSWORD_DEFAULT);
+    }
 }
